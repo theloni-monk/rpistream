a2f8f6e8d38cae6cb17569520b43a80b75ba90de
thatguy1234510 <theoac2009@outlook.com>
2018-07-27 20:50:10 -0400

    added docs to client

--- Raspi_camerastream/streamclient.py
+++ Raspi_camerastream/streamclient.py
@@ -11,14 +11,15 @@ from netutils import *
 class Client:
 
     def __init__(self, **kwargs):
-        #Writing still doesnt work
+        #output file seems to be corrupted: likely due to output file stream not being closed correctly
         self.Write = kwargs.get("WriteFile", False)
         self.writepath = kwargs.get("path", "")
         self.FileFPS = kwargs.get("fileoutFps", 10)
-        self.FileName = kwargs.get("fileName", 'outpy.avi')
-        self.iRes = kwargs.get("imageResolution", (640, 480))
+        self.FileName = kwargs.get("fileName", 'outpy')
+        self.iRes = kwargs.get("imageResolution", (1280, 960))
+        self.resize = kwawrgs.get("resize_cof", 1.0)
         self.out = cv2.VideoWriter(
-            self.writepath+self.FileName, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), self.FileFPS, self.iRes)
+            self.writepath+self.FileName+'.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), self.FileFPS, self.iRes)
 
         self.ip = kwargs.get("serverIp", "18.111.87.85")
         self.s = socket.socket()
@@ -28,6 +29,13 @@ class Client:
         atexit.register(self.close)
 
     def recv(self, size=1024):
+        """Recieves a single frame
+        Args:
+            size: how big a frame should be
+                default: 1024
+        returns:
+            single data frame
+        """
         data = bytearray()
         while 1:
             buffer = self.s.recv(1024)
@@ -35,10 +43,10 @@ class Client:
             if len(buffer) == 1024:
                 pass
             else:
-                #print("recv() data ingested")
                 return data
 
     def startStream(self):
+        """Decodes files from stream and displays them"""
         img = np.zeros((3, 3))
         # initial
         prevFrame = np.load(io.BytesIO(self.D.decompress(recv_msg(self.s))))
@@ -60,11 +68,12 @@ class Client:
                 self.out.write(img)
 
             # show it scaled up
-            cv2.imshow("feed", cv2.resize(img, (0, 0), fx=3.0, fy=3.0))
+            cv2.imshow("feed", cv2.resize(img, (0, 0), fx=self.resize, fy=self.resize))
             if cv2.waitKey(1) == 27:
                 break  # esc to quit
 
     def close(self):
+        """Closes socket and opencv instances"""
         self.out.release()
         self.s.close()
         cv2.destroyAllWindows()
@@ -73,5 +82,5 @@ class Client:
 if __name__ == "__main__":
     ianIp = "10.189.81.154"
     rasPiIp = "18.111.87.85"
-    client = Client(serverIp=rasPiIp, WriteFile=True, port=5000)
+    client = Client(serverIp=rasPiIp, WriteFile=True, resize_cof=1.5, port=5000)
     client.startStream()
