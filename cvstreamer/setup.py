c65f648e63d5f91de642666f234d7869ef804a37
thatguy1234510 <theoac2009@outlook.com>
2018-07-27 23:35:14 -0400

    various

--- cvstreamer/setup.py
+++ cvstreamer/setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open("/README.md", "r") as fh:
-    long_description = fh.read()
+#with open("../README.md", "r") as fh:
+#    long_description = fh.read()
 
 setuptools.setup(
-    name="rpi-stream-img",
+    name="cvstreamer",
     version="0.0.1",
     author="Theo Cooper and Ian Huang",
     author_email="theoac2009@outlook.com",
     description="A very simple library built for streaming video from a remote Raspberry Pi server in realtime.",
-    long_description=long_description,
+    long_description="foo",
     long_description_content_type="text/markdown",
-    url="https://github.com/thatguy1234510/rpi-stream-img",
+    url="https://github.com/thatguy1234510/cvstreamer",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
