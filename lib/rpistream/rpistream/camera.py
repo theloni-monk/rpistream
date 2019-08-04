4b71f4f01717fe8470b2088654c95acf932bd42f
Theo <theo.acooper@gmail.com>
2019-08-03 17:57:35 -0700

    fixed opencv constant problems

--- lib/rpistream/rpistream/camera.py
+++ lib/rpistream/rpistream/camera.py
@@ -9,18 +9,19 @@ class Camera:
         self.deviceId=kwargs.get("device", 0)
         self.cam = cv2.VideoCapture(self.deviceId) #captures from the first webcam it sees by default
         
-        self.trueRes=(self.cam.get(cv2.CV_CAP_PROP_FRAME_WIDTH),self.cam.get(cv2.CV_VAP_PROP_FRAME_HEIGHT))
-        self.scale=(1,1)
-        self.outRes=(self.trueRes[0]*self.scale[0],self.trueRes[1]*self.scale[1])
-
+        self.trueRes=(int(self.cam.get(3)),int(self.cam.get(4)))
+        self.sRes = self.trueRes
         self.output = None
 
     def setRes(self,res):
         """Actually just adjust internal scale value"""
-        self.scale=(res[0]/self.trueRes[0],res[1]/self.trueRes[1])
+        self.sRes = (int(res[0]),int(res[1]))
 
     def getFps(self):
-        return self.cam.get(cv2.CV_CAP_PROP_FPS)
+        try:
+            return self.cam.get(5)
+        except: # error type unclear: it is a underlying opencv error
+            return None
 
     @property
     def image(self):
@@ -29,8 +30,7 @@ class Camera:
             raise Exception("Unable to retrieve image from camera")
         if self.mirror:
             img = cv2.flip(img, 1)
-        self.output = cv2.resize(img,(0,0),self.scale[0],self.scale[1])
-
+        self.output = cv2.resize(img, self.sRes)
         return self.output
 
 if __name__ == "__main__":
