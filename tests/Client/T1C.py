d3a2cc346d9ae5303b0d8990ea7a0ea8c6d85637
Theo <theo.acooper@gmail.com>
2019-08-03 17:58:44 -0700

    removed

--- tests/Client/T1C.py
+++
@@ -1,4 +0,0 @@
-from rpistream.streamclient import Client
-
-client = Client(serverIp="localhost", port = 5000) # Connects to the server
-client.startStream() # Starts recieving data and displaying the video
